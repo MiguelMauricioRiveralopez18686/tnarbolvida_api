d3033d64e850bbd4debbf327670e6ab88c762d14
Miguel Mauricio Rivera López <91698396+MiguelMauricioRiveralopez18686@users.noreply.github.com>
2023-04-02 04:12:22 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const jsonServer = required ("json-server"); 
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
-
-server.use(middlewares);
-server.use(router);
-server.listen(port);
